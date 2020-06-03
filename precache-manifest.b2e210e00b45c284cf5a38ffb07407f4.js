2f37f656d18ebb9cfc994f0193ebbf8f5fefaf05
kingbon <rkingbon@gmail.com>
2020-06-03 20:38:43 +0800

    06-03-2020

---
+++ precache-manifest.b2e210e00b45c284cf5a38ffb07407f4.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bba7588078fa0cf3d57e12c7222b7374",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7a4e8f0b7f2b1a2c71f9",
+    "url": "/static/css/2.9522b9fe.chunk.css"
+  },
+  {
+    "revision": "6c2c3f9d47fc466478f0",
+    "url": "/static/css/main.555528da.chunk.css"
+  },
+  {
+    "revision": "7a4e8f0b7f2b1a2c71f9",
+    "url": "/static/js/2.9f75391c.chunk.js"
+  },
+  {
+    "revision": "a1a636632721a402612df3e043dd630d",
+    "url": "/static/js/2.9f75391c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c2c3f9d47fc466478f0",
+    "url": "/static/js/main.26d2d68d.chunk.js"
+  },
+  {
+    "revision": "967a46d8b67e4e01db1f",
+    "url": "/static/js/runtime-main.7e4b9196.js"
+  },
+  {
+    "revision": "c5309a6c50b8c6793eab0e9d76718c21",
+    "url": "/static/media/CMJ-Product-Catalogue-2.c5309a6c.jpg"
+  },
+  {
+    "revision": "fca2acfaa55f4b80868d2a6ad1b88b7c",
+    "url": "/static/media/CMJ-Product-Catalogue.fca2acfa.jpg"
+  },
+  {
+    "revision": "0078a3e6ccb8801906ff6b2a523487ba",
+    "url": "/static/media/Genecon Construction Website.0078a3e6.jpg"
+  },
+  {
+    "revision": "a3485d2541e96161e7cd0209547b3ccd",
+    "url": "/static/media/HumanTech - Manpower Agency Logo.a3485d25.jpg"
+  },
+  {
+    "revision": "8f79913fed7d6f393dd3327463377399",
+    "url": "/static/media/King Bon Profile Picture.8f79913f.jpg"
+  },
+  {
+    "revision": "2d6757628de6e23d65605f7df35e3345",
+    "url": "/static/media/King-Bon-Graduation-Picture.2d675762.jpg"
+  },
+  {
+    "revision": "b2ecd720796224256c1727457832bd82",
+    "url": "/static/media/Mendira Calling Card.b2ecd720.jpg"
+  },
+  {
+    "revision": "acf3e8a23829f2fafc60333523c3d32c",
+    "url": "/static/media/Mendira Logo 2019.acf3e8a2.jpg"
+  },
+  {
+    "revision": "8681ff94c60f59d58ccc710817ee3b0b",
+    "url": "/static/media/Mendira Logo 2020.8681ff94.jpg"
+  },
+  {
+    "revision": "2bc57ff853dbfb8e6b2a82eecc1e350e",
+    "url": "/static/media/Mendira.net Website 2019.2bc57ff8.jpg"
+  },
+  {
+    "revision": "f376497baac1834bf6d3d21b5b4c3c76",
+    "url": "/static/media/Mendira.net Website 2020.f376497b.jpg"
+  },
+  {
+    "revision": "2e10f90b7406e9784c0bd725fef7935c",
+    "url": "/static/media/Mukbang Bloomy Website.2e10f90b.jpg"
+  },
+  {
+    "revision": "f7f78ade6d88a513bebcf5a76c1c5707",
+    "url": "/static/media/Neozzle - Best Deals Are Here Logo.f7f78ade.jpg"
+  },
+  {
+    "revision": "9f523808879e633e228393807d990c5f",
+    "url": "/static/media/Neozzle Website.9f523808.jpg"
+  }
+]);
